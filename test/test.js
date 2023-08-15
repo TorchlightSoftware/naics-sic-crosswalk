8733446165fe351715058e1eb3ad049202661362
removedporn <86824510+removedporn@users.noreply.github.com>
2023-08-16 01:05:05 +0800

    Delete test directory

--- test/test.js
+++
@@ -1,7 +0,0 @@
-const xw = require('..')
-const _ = require('lodash')
-
-console.log(_.find(xw, {naics: '111120'}).sic)
-console.log(_.find(xw, {naics: '111120'}).sicDescription)
-console.log(_.find(xw, {sic: '0119'}).naics)
-console.log(_.find(xw, {sic: '0119'}).naicsDescription)
